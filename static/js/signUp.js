efe38277acf24f3a3361256a6c3b7c2229851471
Filip <zgchuck@gmail.com>
2019-08-07 01:35:10 +0200

    massive changes

--- static/js/signUp.js
+++ static/js/signUp.js
@@ -1,13 +1,25 @@
 $(function(){
 	$('#btnSignUp').click(function(){
-		
+
 		$.ajax({
 			url: '/signUp',
 			data: $('form').serialize(),
 			type: 'POST',
 			success: function(response){
-				console.log(response);
-				$("#status").append(response);
+				html = jQuery.parseJSON(response);
+				if (typeof html.message != "undefined") {
+					$("#status").empty().append(html.message);
+					console.log(html.message);
+				}
+				else if (typeof html.error != "undefined") {
+					$("#status").empty().append(html.error);
+					console.log(html.error);
+				}
+				else if (typeof html.html != "undefined") {
+					$("#status").empty().append(html.html);
+					console.log(html.html);
+				}
+				console.log(response)
 			},
 			error: function(error){
 				console.log(error);
